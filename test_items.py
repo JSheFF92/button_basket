774632c63fbebda5cbbc3898a0200a378e3aef09
Evgenyi <shevchuk291992@gmail.com>
2023-04-08 18:21:44 +0300

    up link to test_items

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_190/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_guest_should_see_login_link_pass(browser):
