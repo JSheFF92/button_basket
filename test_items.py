313b49ca0d7a3ae325d118157edfb1c6460f8b10
Evgenyi <shevchuk291992@gmail.com>
2023-04-08 18:39:55 +0300

    rename func

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link_pass(browser):
+def test_get_basket_button(browser):
     browser.get(link)
     basket_button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert basket_button, 'Basket button not found'
